629e6a99ea0aa3e4f0f5e971f7115ea8f01bdb60
landen3080 <zhangyu@1234-2.local>
2018-04-29 02:07:28 +0800

    search.js單獨分開

--- search.js
+++ search.js
@@ -1,20 +1,20 @@
 function mysearch() {
   // 声明变量
-  var input, filter, table, tr, td, i;
+  var input, filter, table, tr, td, i, th;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
+  th = table.getElementsByTagName("th");
  
   // 循环表格每一行，查找匹配项
-  for (i = 0; i < tr.length; i++) {
-    for(x = 0; x < td.length; x++){
+  for (i = 1; i < tr.length; i++) {
+    tr[i].style.display = "none";
+    for(x = 0; x < th.length; x++){
       td = tr[i].getElementsByTagName("td")[x];
       if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
         }
       } 
     }
